8c2fe72938421ab07d4612d3b3f19eb779c4cfa2
Markus Lasermann <markus.lasermann@ptvgroup.com>
2016-09-01 16:32:44 +0200

    the actual profile, loading all the other files

---
+++ profile.ps1
@@ -0,0 +1,6 @@
+﻿# My PowerShell Profile
+. "$PSScriptRoot\aliases.ps1"
+. "$PSScriptRoot\avoid_accidental_dcommit.ps1"
+. "$PSScriptRoot\everything.ps1"
+. "$PSScriptRoot\prompt.ps1"
+Import-Module PSColor
