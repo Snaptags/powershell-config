a7339aef2c9877496fa1417c03eff7de08e445e4
Markus Lasermann <markus.lasermann@ptvgroup.com>
2016-12-23 13:10:27 +0100

    better history search

--- PSReadLine.ps1
+++ PSReadLine.ps1
@@ -2,4 +2,19 @@ if ($host.Name -eq 'ConsoleHost')
 {
     Import-Module PSReadline
     Set-PSReadlineOption -EditMode vi -ViModeIndicator Cursor
+    Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+    Set-PSReadlineKeyHandler -Key UpArrow `
+                             -BriefDescription HistorySearchBackward `
+                             -LongDescription "search history backwards and enter vi normal mode" `
+                             -ScriptBlock {
+                                 [Microsoft.PowerShell.PSConsoleReadLine]::HistorySearchBackward()
+                                 [Microsoft.PowerShell.PSConsoleReadLine]::ViCommandMode()
+                             }
+    Set-PSReadlineKeyHandler -Key DownArrow `
+                             -BriefDescription HistorySearchForward `
+                             -LongDescription "search history forward and enter vi normal mode" `
+                             -ScriptBlock {
+                                 [Microsoft.PowerShell.PSConsoleReadLine]::HistorySearchForward()
+                                 [Microsoft.PowerShell.PSConsoleReadLine]::ViCommandMode()
+                             }
 }
