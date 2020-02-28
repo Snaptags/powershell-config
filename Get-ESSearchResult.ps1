1312cf80bee83e72777e51121519d6e081cae6eb
Markus Lasermann <markus.lasermann@ptvgroup.com>
2020-02-28 10:32:18 +0100

    don't exit the shell on error, works with chocolatey es.exe path

--- Get-ESSearchResult.ps1
+++ Get-ESSearchResult.ps1
@@ -10,50 +10,68 @@ filter Get-FileSize {
 	)
 }
 
-function Get-ESItemProperty {
-    Param
-        (
-         $record
-        )
-    if ((Test-Path $record -ErrorAction SilentlyContinue) -eq $true) {
-        return $record | Get-ItemProperty;
-    }
-    $result = New-Object System.Object
-    $result | Add-Member -type NoteProperty -name Name -value (Split-Path "$record" -leaf)
-    $result | Add-Member -type NoteProperty -name DirectoryName -value (Split-Path "$record")
-    $result | Add-Member -type NoteProperty -name LastWriteTime -value "?"
-    return $result
-}
-
 function Get-ESSearchResult {
+     <#    
+    .SYNOPSIS
+        PowerShell wrapper around Everything search command line (es.exe).
+    .DESCRIPTION
+        Get-ESSearchResult function (alias search) searches for all items containing the search term (SearchTerm parameter is the only mandatory parameter). 
+        The search results (if multiple) are piped to Out-GridView with the -PassThru option enabled so that the result can be seen in GUI and one or multiple items 
+        from within the search results can be selected. By default (no switches turned on) the selected item(s) 
+        are converted to FileSystemInfo objects and their Name, DirectoryName, FileSize and LastModifiedDate are output. 
+        The resulting objects can be used for further processing (copying, deleting….)
+	.PARAMETER SearchTerm
+		The search term to search for see es.exe syntax for options.
+	.PARAMETER OpenItem
+		Switch enabled = Invoke the selected item(s) (only applies to files not folders).
+    .PARAMETER CopyFullPath
+		Switch enabled = Copy the full Path of the selected item to the clipboard.
+    .PARAMETER OpenFolder
+		Switch enabled = Opens the folder(s) that contain(s) the selected item(s) in windows explorer
+    .PARAMETER AsObject
+		Switch enabled = Similar to default output but the full FileSystemInfo objects related to the selected item(s) are output
+	.EXAMPLE
+		search 'mySearchTerm'
+    .LINK
+        https://powershellone.wordpress.com/2015/02/28/using-everything-search-command-line-es-exe-via-powershell/
+    .LINK
+        https://www.voidtools.com/support/everything/command_line_interface/
+    #>
     [CmdletBinding()]
+    [Alias("search")]
     Param
     (
-        #searchterm
         [Parameter(Mandatory=$true, Position=0)]
         $SearchTerm,
-        #openitem
         [switch]$OpenItem,
         [switch]$CopyFullPath,
         [switch]$OpenFolder,
         [switch]$AsObject
     )
-    $esPath = 'C:\Program Files*\es\es.exe'
-    if (!(Test-Path (Resolve-Path $esPath).Path)){
-        Write-Warning "Everything commandline es.exe could not be found on the system please download and install via http://www.voidtools.com/es.zip"
-        exit
+    $esPaths = @('C:\Program Files*\es\es.exe','c:\ProgramData\chocolatey\bin\es.exe')
+    foreach($esPath in $esPaths){
+        $esPath = (Resolve-Path $esPath -ErrorAction SilentlyContinue)
+        if (!$esPath -or !(Test-Path $esPath.Path)){
+            $esPath = ""
+        } else {
+            break
+        }
     }
-	$result = & (Resolve-Path $esPath).Path $SearchTerm
+    if ($esPath -eq ""){
+        Write-Warning "Everything commandline es.exe could not be found on the system please download and install via http://www.voidtools.com/es.zip or using Chocolatey ('choco install everything')"
+        break
+    }
+    $result = & $esPath.Path $SearchTerm
     if($result.Count -gt 1){
-        #$result = $result | Out-GridView -PassThru
+        $result = $result | Out-GridView -PassThru
     }
     foreach($record in $result){
         switch ($PSBoundParameters){
 	        { $_.ContainsKey("CopyFullPath") } { $record | clip }
 	        { $_.ContainsKey("OpenItem") }     { if (Test-Path $record -PathType Leaf) {  & "$record" } }
 	        { $_.ContainsKey("OpenFolder") }   {  & "explorer.exe" /select,"$(Split-Path $record)" }
-	        { $_.ContainsKey("AsObject") }     { Get-ESItemProperty($record) }
-	        default                            { Get-ESItemProperty($record) |
+	        { $_.ContainsKey("AsObject") }     { $record | Get-ItemProperty }
+	        default                            { $record | Get-ItemProperty | 
                                                     select Name,DirectoryName,@{Name="Size";Expression={$_.Length | Get-FileSize }},LastWriteTime
                                                }
         }
